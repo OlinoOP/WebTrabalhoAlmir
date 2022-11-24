e43a503fe9a2f46f6d7ca9956d8374326a53ef61
Bruno Oliveira Alves <brunooliveira150@hotmail.com>
2022-11-24 15:46:45 -0300

    Segundo Commit

--- Front/BCI/src/app/transferencia/transferencia.component.ts
+++ Front/BCI/src/app/transferencia/transferencia.component.ts
@@ -4,7 +4,6 @@ import { TranferenciaService } from './transferencia.service';
 import { Transferencias } from './transferencias';
 
 
-
 var transferenciasRecebidas: DadosTransferencia[];
 var transferenciasEnviadas: DadosTransferencia[];
 
